224dba543a380d6294128a240e30fc6804c003b8
Pablo Massuh <pablomassh@gmail.com>
2019-07-27 14:59:14 -0300

    Created Board component. Draw static view of how the game will look likes.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="container">
+
+      <div class="row text-center mb-4">
+        <div class="col-12">
+          <h1>Welcome to minesweeper game!</h1>
+        </div>
+      </div>
+
+      <div class="row">
+
+        <div class="col text-center">
+          <button type="button" class="btn btn-secondary btn-lg btn-block">Start a new Game!</button>
+        </div>
+
+        <div class="col text-center">
+          <Board />
+        </div>
+
+        <div class="col">
+
+          <h4>References:</h4>
+
+          <div class="ml-2">
+
+            <button type="button" class="btn btn-primary" disabled></button> = Revealed Cell
+            <br/>
+            <button type="button" class="btn btn-primary"></button> = Unrevealed Cell
+
+            <div class="alert alert-info mt-3" role="alert">
+              Only <strong>Unrevealed</strong> cells are clickable!
+            </div>
+          </div>
+        </div>
+
+
+
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Board from './components/Board.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Board
   }
 }
 </script>
@@ -21,7 +60,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Board.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="hello">
+
+
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+    </div><br/>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" ></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" ></button>
+      <button type="button" class="btn btn-primary" ></button>
+      <button type="button" class="btn btn-primary" ></button>
+    </div><br/>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+    </div><br/>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" ></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" ></button>
+      <button type="button" class="btn btn-primary" ></button>
+      <button type="button" class="btn btn-primary" ></button>
+    </div><br/>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+    </div><br/>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" ></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" ></button>
+      <button type="button" class="btn btn-primary" ></button>
+      <button type="button" class="btn btn-primary" ></button>
+    </div><br/>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+    </div><br/>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" ></button>
+      <button type="button" class="btn btn-primary" disabled></button>
+      <button type="button" class="btn btn-primary"></button>
+      <button type="button" class="btn btn-primary" ></button>
+      <button type="button" class="btn btn-primary" ></button>
+      <button type="button" class="btn btn-primary" ></button>
+    </div><br/>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Board',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
